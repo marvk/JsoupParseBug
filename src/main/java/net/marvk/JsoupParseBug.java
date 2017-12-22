41327a2876662f47eaa628cd3a3720300f4f66d3
Marvin <marvk42@gmail.com>
2017-12-22 04:23:39 +0100

    Added some examples of pages that were bad previously

--- src/main/java/net/marvk/JsoupParseBug.java
+++ src/main/java/net/marvk/JsoupParseBug.java
@@ -1,10 +1,15 @@
 package net.marvk;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -42,11 +47,12 @@ public class JsoupParseBug {
 
     /**
      * Use to find and return the first bad url.
+     *
      * @return the url with the smallest page index either with or without trailing slash
      */
     public static String findFirstBadPage() {
         final ExecutorService executorService = Executors.newFixedThreadPool(25);
-        final List<Future<String>> futures = getDownloadFutures(executorService);
+        final List<Future<String>> futures = getDownloadFutures(executorService, false);
 
         for (final Future<String> future : futures) {
             try {
@@ -66,10 +72,12 @@ public class JsoupParseBug {
 
     /**
      * Use to print all bad URLs, both with and without trailing slash.
+     *
+     * @param saveToDisk if parameter is set, saves all bad pages found to badPages/
      */
-    public static void printAllBadUrls() {
+    public static void printAllBadUrls(final boolean saveToDisk) {
         final ExecutorService executorService = Executors.newFixedThreadPool(25);
-        getDownloadFutures(executorService);
+        getDownloadFutures(executorService, saveToDisk);
     }
 
     private static void print(final Document document) {
@@ -82,30 +90,35 @@ public class JsoupParseBug {
         System.out.println(System.lineSeparator());
     }
 
-    private static List<Future<String>> getDownloadFutures(final ExecutorService executorService) {
+    private static List<Future<String>> getDownloadFutures(final ExecutorService executorService, final boolean save) {
         return IntStream.range(1, 4000)
-                        .mapToObj(page -> Arrays.asList(download(executorService, URL + page), download(executorService, URL + page + "/")))
+                        .mapToObj(page -> Arrays.asList(download(executorService, URL + page, save), download(executorService, URL + page + "/", save)))
                         .flatMap(Collection::stream)
                         .collect(Collectors.toList());
     }
 
-    private static Future<String> download(final ExecutorService executor, final String url) {
+    private static Future<String> download(final ExecutorService executor, final String url, final boolean save) {
         return executor.submit(() -> {
-            try {
-                final List<String> collect = Jsoup.connect(url)
-                                                  .get()
-                                                  .select("item")
-                                                  .stream()
-                                                  .map(e -> e.select("pubDate"))
-                                                  .flatMap(Collection::stream)
-                                                  .map(Element::text)
-                                                  .collect(Collectors.toList());
+            final Connection.Response response = Jsoup.connect(url)
+                                                      .execute();
 
+
+            final List<String> collect = response.parse()
+                                                 .select("item")
+                                                 .stream()
+                                                 .map(e -> e.select("pubDate"))
+                                                 .flatMap(Collection::stream)
+                                                 .map(Element::text)
+                                                 .collect(Collectors.toList());
+            try {
                 for (final String s : collect) {
                     LocalDateTime.parse(s, DateTimeFormatter.RFC_1123_DATE_TIME);
                 }
-            } catch (final DateTimeParseException t) {
+            } catch (final DateTimeParseException e) {
                 System.out.println(url);
+                if (save) {
+                    saveToFile(url, Jsoup.connect(url).execute().bodyAsBytes());
+                }
                 return url;
             }
 
@@ -113,7 +126,25 @@ public class JsoupParseBug {
         });
     }
 
+    private static void saveToFile(final String url, final byte[] bytes) {
+        try {
+            final Path slash = Paths.get("badPages" + File.separator + url.replaceFirst(URL, "")
+                                                                          .replaceAll("/", "SLASH"));
+
+            if (!slash.getParent().toFile().exists()) {
+                Files.createDirectory(slash.getParent());
+            }
+
+            Files.write(
+                    slash,
+                    bytes
+            );
+        } catch (final Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
     public static void main(final String[] args) throws IOException {
-        printBadExampleOutput();
+        printAllBadUrls(true);
     }
 }
